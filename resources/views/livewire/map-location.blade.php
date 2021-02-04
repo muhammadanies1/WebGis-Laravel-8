f9f15e6f4998ed4bc0d7ce2201ca6c4f9ff82ac0
aniesUbuntu20 <muhammadanies0@gmail.com>
2021-02-04 10:58:38 +0700

    menambahkan dan menampilkan banyak marker kedalam maps

--- resources/views/livewire/map-location.blade.php
+++ resources/views/livewire/map-location.blade.php
@@ -58,7 +58,123 @@
                 zoom: 11.15
             });
 
-            console.log("ini value dari livewire", @this.test);
+            /// data tentang titik koordinat pera dll
+            const geoJson = {
+                "type"      : "FeatureCollection",
+                "features"  : [
+                    {
+                        "type"      : "Feature",
+                        "geometry"  : {
+                            "coordinates"   : ["110.68130321458278","-6.889397496088364"],
+                            "type"          : "Point"
+                    },
+                        "properties": {
+                            "message"       : "Mantap",
+                            "iconSize"      : [50,50],
+                            "locationId"    : 30,
+                            "title"         : "Hello new",
+                            "image"         : "1a1eb1e4106fff0cc3467873f0f39cab.jpeg",
+                            "description"   : "Mantap"
+                    }
+                },
+                    {
+                        "type"      : "Feature",
+                        "geometry"  : {
+                            "coordinates"   : ["110.6101366627027","-6.866972414530693"],
+                            "type"          : "Point"
+                        },
+                        "properties": {
+                            "message"       : "oke mantap Edit",
+                            "iconSize"      : [50,50],
+                            "locationId"    : 29,
+                            "title"         : "Rumah saya Edit",
+                            "image"         : "0ea59991df2cb96b4df6e32307ea20ff.png",
+                            "description"   : "oke mantap Edit"
+                        }
+                },
+                    {
+                        "type"          : "Feature",
+                        "geometry"      : {
+                            "coordinates"   : ["110.55041864308163","-6.8540698335605725"],
+                            "type"          : "Point"
+                        },
+                        "properties"    : {
+                            "message"       : "Update Baru",
+                            "iconSize"      : [50,50],
+                            "locationId"    : 22,
+                            "title"         : "Update Baru Gambar",
+                            "image"         : "d09444b68d8b72daa324f97c999c2301.jpeg",
+                            "description"   : "Update Baru"
+                        }
+                },
+                    {
+                        "type"      : "Feature",
+                        "geometry"  : {
+                            "coordinates"   : ["110.55536935973322","-6.8995345138881135"],
+                            "type"          : "Point"
+                        },
+                        "properties": {
+                            "message"       : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                            "iconSize"      : [50,50],
+                            "locationId"    : 19,
+                            "title"         : "awdwad",
+                            "image"         : "f0b88ffd980a764b9fca60d853b300ff.png",
+                            "description"   : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                        }
+                },
+                    {
+                        "type"      : "Feature",
+                        "geometry"  : {
+                            "coordinates"   : ["110.59714103148934","-6.873423573841663"],
+                            "type"          : "Point"
+                        },
+                        "properties": {
+                            "message"       : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                            "iconSize"      : [50,50],
+                            "locationId"    : 18,
+                            "title"         : "adwawd",
+                            "image"         : "4c35cb1b76af09e6205f94024e093fe6.jpeg",
+                            "description"   : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+                        }
+                },
+                    {
+                        "type"      : "Feature",
+                        "geometry"  : {
+                            "coordinates"   : ["110.73328573943331","-6.926258337937568"],
+                            "type"          : "Point"
+                        },
+                        "properties": {
+                            "message"       : "awdwad",
+                            "iconSize"      : [50,50],
+                            "locationId"    : 12,
+                            "title"         : "adawd",
+                            "image"         : "7c8c949fd0499eb50cb33787d680778c.jpeg",
+                            "description"   : "awdwad"
+                        }
+                }
+            ]
+            }
+
+            ///untuk menampilkan data geoJson
+            const loadLocations = () => {
+                geoJson.features.forEach((location) => {
+                    const {geometry, properties} = location
+                    const {iconSize, locationId, title, image, description} = properties
+
+                    let markerElement                   = document.createElement('div')
+                    markerElement.className             = 'marker' + locationId
+                    markerElement.id                    = locationId
+                    markerElement.style.backgroundImage = 'url(https://docs.mapbox.com/help/demos/custom-markers-gl-js/mapbox-icon.png)'
+                    markerElement.style.backgroundSize  = 'cover'
+                    markerElement.style.width           = '50px'
+                    markerElement.style.height          = '50px'
+
+                    new mapboxgl.Marker(markerElement)
+                    .setLngLat(geometry.coordinates)
+                    .addTo(map)
+                })
+            }
+            loadLocations()
 
             /// Style dari maps yang digunakan
             const style = "dark-v10" /// varian lain dari style map: light-v10, outdoors-v11 satellite-v9, streets-v11, dark-v10
